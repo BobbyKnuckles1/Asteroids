69b1a97f2fae00dc72797c65c8b64d138e66da0b
Bobby <144020667+BobbyKnuckles1@users.noreply.github.com>
2025-08-05 22:29:56 +0100

    added bullets and asteroid destruction logic

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from bullet import Shot
+import sys
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    clock = pygame.time.Clock()
+    asteroid_field = AsteroidField()
+    
     dt = 0
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    black = (0, 0, 0)
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,7 +33,17 @@ def main():
             
         updatable.update(dt)
         
-        screen.fill(black)
+        for asteroid in asteroids:
+            if asteroid.collision(player) == True:
+                sys.exit(["Game Over!"])
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collision(shot) == True:
+                    asteroid.kill()
+                    shot.kill()
+        
+        screen.fill("black")
         
         for object in drawable:
             object.draw(screen)
