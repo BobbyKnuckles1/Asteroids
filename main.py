fa21328d061b44ec70b4dd80f32d40fde991adc7
Bobby <144020667+BobbyKnuckles1@users.noreply.github.com>
2025-08-05 19:02:52 +0100

    movement implemented, grouped classes

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from player import Player
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
@@ -17,11 +21,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+        
         screen.fill(black)
-        player.draw(screen)
+        
+        for object in drawable:
+            object.draw(screen)
+        
         pygame.display.flip()
-        clock.tick(60)
+ 
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
